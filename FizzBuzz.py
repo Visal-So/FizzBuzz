92c70651cd5ee2527257c5b0d86a6a43e7b03284
Visal-So <77860968+Visal-So@users.noreply.github.com>
2022-10-12 10:51:46 -0500

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,13 @@
+count = 0
+
+while count < 100:
+    count = count + 1
+
+    if count % 3 == 0 and count % 5 == 0:
+        print("FizzBuzz")
+    elif count % 3 == 0:
+        print("Fizz")
+    elif count % 5 == 0:
+        print("Buzz")
+    else:
+        print(count)
